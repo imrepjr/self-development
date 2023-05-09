d44f842a5b954157218f96945a50fb2cf9a529ce
Imre Penzes <emery90@gmail.com>
2023-05-09 20:16:44 +0200

    Add angular recipe app

---
+++ packages/angular-recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.scss'],
+})
+export class ShoppingListComponent {
+  ingredients: Array<Ingredient> = [
+    new Ingredient('apples', 5),
+    new Ingredient('tomato', 2),
+  ];
+}
