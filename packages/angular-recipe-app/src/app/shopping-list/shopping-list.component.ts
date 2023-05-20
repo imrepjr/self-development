4558e1291f5de646430a36bfd4eba776b1c9b30b
Imre Penzes <emery90@gmail.com>
2023-05-20 14:57:43 +0200

    [Angular] Implement adding ingredients to shopping list

--- packages/angular-recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ packages/angular-recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -11,4 +11,8 @@ export class ShoppingListComponent {
     new Ingredient('apples', 5),
     new Ingredient('tomato', 2),
   ];
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
