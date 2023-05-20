7f529d072298fbae44d0ec65d0e267e053150545
Imre Penzes <emery90@gmail.com>
2023-05-20 14:57:43 +0200

    [Angular] Implement adding ingredients to shopping list

--- packages/angular-recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ packages/angular-recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.scss']
+  styleUrls: ['./shopping-edit.component.scss'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
 
+  @Output('onAddIngredient') addIngredientEvent =
+    new EventEmitter<Ingredient>();
+
+  addIngredient() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = +this.amountInput.nativeElement.value;
+    const newIngredient = new Ingredient(name, amount);
+
+    this.addIngredientEvent.emit(newIngredient);
+  }
 }
