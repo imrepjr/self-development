c5d12f9624a160d1342d193ce0a351fb09e49791
Imre Penzes <emery90@gmail.com>
2023-05-20 14:29:01 +0200

    [Angular] update recipe feature

--- packages/angular-recipe-app/src/app/recipes/recipes.component.ts
+++ packages/angular-recipe-app/src/app/recipes/recipes.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  styleUrls: ['./recipes.component.scss'],
 })
 export class RecipesComponent {
+  selectedRecipe?: Recipe;
 
+  selectRecipe(recipe: Recipe) {
+    this.selectedRecipe = recipe;
+  }
 }
