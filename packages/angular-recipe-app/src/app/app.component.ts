be43ad0978bc36433a622757d3a7dcfda4c8e539
Imre Penzes <emery90@gmail.com>
2023-05-20 14:28:20 +0200

    [Angular] add state navigation

--- packages/angular-recipe-app/src/app/app.component.ts
+++ packages/angular-recipe-app/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-recipe-app';
+  activePage = 'recipes';
+
+  setActivePage(newPage: string) {
+    this.activePage = newPage;
+  }
 }
--- packages/angular-recipe-app/src/app/header/header.component.ts
+++ packages/angular-recipe-app/src/app/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @Output('onNavigation') navigationEvent = new EventEmitter<string>();
 
+  handleNavigation(target: string) {
+    this.navigationEvent.emit(target);
+  }
 }
