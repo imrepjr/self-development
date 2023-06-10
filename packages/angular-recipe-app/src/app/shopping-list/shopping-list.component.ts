e98f5e38d3859c1e306ead041cd40d9d38ae8232
Imre Penzes <emery90@gmail.com>
2023-06-10 11:39:45 +0200

    Remove unused imports

--- packages/angular-recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ packages/angular-recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
