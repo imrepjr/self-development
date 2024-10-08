d9274f3fb7457edc9c84b65b91bc7ebc04bc0a02
Imre Penzes <emery90@gmail.com>
2024-10-08 15:04:56 +0200

    Add routing foundations

--- packages/angular-recipe-app/src/app/app.module.ts
+++ packages/angular-recipe-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     ShoppingEditComponent,
     DropdownDirective,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- packages/angular-recipe-app/src/app/header/header.component.ts
+++ packages/angular-recipe-app/src/app/header/header.component.ts
@@ -1,14 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  @Output('onNavigation') navigationEvent = new EventEmitter<string>();
-
-  handleNavigation(target: string) {
-    this.navigationEvent.emit(target);
-  }
-}
+export class HeaderComponent {}
